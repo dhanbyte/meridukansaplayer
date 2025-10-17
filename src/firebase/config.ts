26707dfaffcd1d003529aea8d4976bcd6cbfca48
Dhananjay Singh <dhananjay.win2004@gmail.com>
2025-10-17 03:23:20 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,15 @@
 import { getApp, getApps, initializeApp, type FirebaseOptions } from 'firebase/app';
 
-const firebaseConfig: FirebaseOptions = process.env.NEXT_PUBLIC_FIREBASE_CONFIG
-  ? JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG)
-  : {};
+const firebaseConfig: FirebaseOptions = {
+  apiKey: "AIzaSyAq8Zk7tueHQaIZ_kgApbje6EhJ4dAX4m8",
+  authDomain: "manishprajapati-a10cd.firebaseapp.com",
+  projectId: "manishprajapati-a10cd",
+  storageBucket: "manishprajapati-a10cd.appspot.com",
+  messagingSenderId: "817023797484",
+  appId: "1:817023797484:web:cc07447197b3a8b1ad7dc9",
+  measurementId: "G-J2B9T4LZPG"
+};
+
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
